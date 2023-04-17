9237f7972a0d068c9f19e378fcb24e739ae1222f
Gruzin <gruzd.roman@inbox.ru>
2023-04-17 22:45:07 +0300

    Commit

--- tasks/task.py
+++ tasks/task.py
@@ -1,20 +1,19 @@
 class HistoryDict:
     my_dict = {}
     new_list = []
-    def __init__(self, my_dict):
+    def __init__(self, my_dict ):
         self.new_dict = my_dict
+        self.new_list = []
 
     def set_value(self, key, value):
         self.my_dict[key] = value
         self.new_list.append(key)
-        return
+        if len(self.new_list) > 5:
+            self.new_list.pop(0)
 
     def get_history(self):
-        self.new_list.reverse()
-        while len(self.new_list) > 5:
-            self.new_list.pop()
-        self.new_list.reverse()
         return self.new_list
+
 
 
 
